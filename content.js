97d7b8f77650122f7b2c666475537f6334aface2
Nguyễn Ngọc Thông <thongnguyenngoc3738@gmail.com>
2024-01-02 15:27:17 +0700

    update

--- content.js
+++ content.js
@@ -12,7 +12,7 @@ fetch(apiUrl)
     return response.text();
   })
   .then(data => {
-    console.log(data); // Xử lý dữ liệu trả về ở đây
+    console.log("data", data); // Xử lý dữ liệu trả về ở đây
     if (data === 'benign') {
       // Hiển thị thông báo khi trang được xác định là an toàn
       alert('Trang web này an toàn!');
@@ -20,7 +20,7 @@ fetch(apiUrl)
       // Hiển thị thông báo và chặn truy cập nếu trang được xác định là độc hại
       alert('Trang web này có thể độc hại và đã bị chặn!');
       // Chuyển hướng người dùng đến trang cảnh báo hoặc an toàn khác nếu cần
-      window.location.href = 'https://trangcanhbao.com';
+      window.location.href = 'https://chongluadao.vn';
     } else {
       // Xử lý trường hợp khác nếu cần thiết
     }
